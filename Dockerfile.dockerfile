d060fe8e0b7a06f013058c18561c17cc94bb5aef
Sam Povilus <povilus@povil.us>
2023-02-24 09:29:17 -0700

    adding todo cleaning up

--- Dockerfile.dockerfile
+++ Dockerfile.dockerfile
@@ -4,13 +4,14 @@ FROM python:3.8-slim-buster
 WORKDIR /app
 
 
-# docker rm /ephemeris_timelapse; docker build --pull --rm -f "Dockerfile.dockerfile" -t ephemeristimelapse:latest "." ; docker run  -v '/mnt/user/security_cams/':'/timelapse/':'rw' --name ephemeris_timelapse -e PYTHONUNBUFFERED=0 -e LOGLEVEL=DEBUG -e LONGITUDE=-105.23 -e LATITUDE=40.0 -e TZ=America/Denver -e INPUT_ROOT="/timelapse/FVSYkiwp2G" ephemeristimelapse:latest 
+# docker rm /ephemeris_timelapse; docker build --pull --rm -f "Dockerfile.dockerfile" -t ephemeristimelapse:latest "." && docker run  -v '/mnt/user/security_cams/':'/timelapse/':'rw' --name ephemeris_timelapse -e PYTHONUNBUFFERED=0 -e LOGLEVEL=DEBUG -e LONGITUDE=-105.23 -e LATITUDE=40.0 -e TZ=America/Denver -e INPUT_ROOT="/timelapse/FVSYkiwp2G" -e OUTPUT_ROOT="/timelapse/" -e ALLOWED_TIMEDIFF_SEC=60.0 ephemeristimelapse:latest 
 ENV LOGLEVEL=INFO
-ENV LONGITUDE
-ENV LATITUDE
-ENV TZ
-ENV INPUT_ROOT
-ENV OUTPUT_ROOT
+ENV LONGITUDE=
+ENV LATITUDE=
+ENV TZ=
+ENV INPUT_ROOT=
+ENV OUTPUT_ROOT=
+ENV ALLOWED_TIMEDIFF_SEC=
 COPY requirements.txt requirements.txt
 RUN pip3 install -r requirements.txt
 
--- ephemeris_timelapse.py
+++ ephemeris_timelapse.py
@@ -17,6 +17,8 @@ o = ephem.Observer()
 o.lat, o.long = os.environ.get("LATITUDE"), os.environ.get("LONGITUDE")
 sun = ephem.Sun()
 
+allowed_timediff_sec = float(os.environ.get("ALLOWED_TIMEDIFF_SEC"))
+
 input_root_path = os.environ.get("INPUT_ROOT")
 capture_dirs = os.listdir(input_root_path)
 timelapse_dirs = [timelapse_dirs for timelapse_dirs in capture_dirs if "_timelapse" in timelapse_dirs]
@@ -27,7 +29,7 @@ if not isinstance(numeric_level, int):
     raise ValueError('Invalid log level: %s' % loglevel)
 logging.basicConfig(level=numeric_level)
 
-logging.info("Longitue is: " + str(float(o.long)) +" Latitide: " + str(float(o.lat)) + " root path is: " + input_root_path + " logging level is " + str(numeric_level))
+logging.info("Longitue is: " + str(float(o.long)) +" Latitide: " + str(float(o.lat)) + " root path is: " + input_root_path + " logging level is " + str(numeric_level) + " allowed timediff is: " + str(allowed_timediff_sec))
 logging.debug("datetime.now: " + str(datetime.now()) + " ephem.localtime(o.next_transit(sun)): " + str(ephem.localtime(o.next_transit(sun))) + " os.system(\"date\"): " + str(osdate) )
 
 def closest(lst, K):
@@ -35,6 +37,7 @@ def closest(lst, K):
 
 while True:
     for camera in timelapse_dirs:
+        #TODO: allow for offsetting from solar noon
         noon_cam_path = str(output_root_path) + str(camera) + "_noon/"
         isExist = os.path.exists(noon_cam_path)
         if not isExist:
@@ -53,14 +56,15 @@ while True:
                 pic_filename = (re.sub(regex,subst,str(closest_pic)))
                 pic_path = str(input_root_path + "/" + camera + "/" + day + "/" +pic_filename)
                 logging.debug("Pic path is: " + pic_path +  " difference is: " + str(time_diff))
-                if(time_diff < 60):
+                if(abs(time_diff) < allowed_timediff_sec):
                     if not (os.path.exists(noon_cam_path + pic_filename)):
-                        #TODO: I belelvie this overwrites, I should modify to not copy  if already exsists
                         shutil.copy2(pic_path,noon_cam_path + pic_filename)
-                        logging.info("found a close enough picture copying: " + pic_path + " to: " + noon_cam_path + pic_filename)
-                    #else: 
-                        logging.debug("no picture close enough, not copying " + noon_cam_path + pic_filename)
+                        logging.info("Found a close enough picture copying: " + pic_path + " to: " + noon_cam_path + pic_filename)
+                    else: 
+                        logging.debug("Picture already exsists, not copying " + noon_cam_path + pic_filename)
+                else:
+                    logging.debug("No picture less than " + str(allowed_timediff_sec) + " seconds from solar noon, not copying, closest picture is: " + str(closest_pic))
+    #TODO: pause until midnight localtime
     pause_until = datetime.now() + timedelta(hours=23)
-    logging.info(pause_until)
+    logging.info("Sleeping until: " + str(pause_until))
     pause.until(pause_until)
-    
